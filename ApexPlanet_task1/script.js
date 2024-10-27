c3f8b3c3ad2da058da7ecd6074a031d5ede1258d
PALADI-PUJITHA <paladipujitha@gmail.com>
2024-10-27 19:23:32 +0530

    Add files via upload

---
+++ ApexPlanet_task1/script.js
@@ -0,0 +1,3 @@
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert("Ratan Tata has transformed the Tata Group into a global enterprise with a focus on innovation and sustainability.");
+});
